3b47e91ed492f5d42c3ddc1c245ac7b3e504f596
one95nick <97886108+one95nick@users.noreply.github.com>
2022-11-03 21:25:15 +0500

    master

---
+++ source/js/index.js
@@ -0,0 +1,4 @@
+const footerMenu = document.querySelector('.footer__menu');
+const footerToggle = document.querySelector('.footer__toggle');
+
+footerToggle.addEventListener('click', () => footerMenu.classList.toggle('is-active'));
